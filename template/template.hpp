b212e910dccebc634f631abe2ca601af4847c00b
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2023-04-19 19:08:56 +0900

    make rollinghash, template

---
+++ template/template.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include<bits/stdc++.h>
+#include<iostream>
+#define rep(i,s,n) for (int i = s; i < (int)(n); i++)
+#define all(v) v.begin(),v.end()
+
+template<typename T> bool chmin(T &a, const T &b){
+    if (a <= b) return false;
+    a = b;
+    return true;
+}
+template<typename T> bool chmax(T &a, const T &b){
+    if (a >= b) return false;
+    a = b;
+    return true;
+}
