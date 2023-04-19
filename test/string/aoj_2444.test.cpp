794a6f4e9908806a866218e9e0bc8c088cd592d0
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-04-20 00:09:48 +0900

    add test about RollingHash

---
+++ test/string/aoj_2444.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2444&lang=jp"
+
+#include "../../template/template.hpp"
+#include "../../string/RollingHash.hpp"
+
+int main() {
+    int n,m;
+    std::string s;
+    std::cin >> n >> m >> s;
+    lib::RollingHash rh(s);
+    int l = 0, r = 1;
+    std::set<ull> set;
+    rep(i,0,m) {
+        std::string q;
+        std::cin >> q;
+        if(q == "L++") {
+            l++;
+        }
+        else if(q == "L--") {
+            l--;
+        }
+        else if(q == "R++") {
+            r++;
+        }
+        else {
+            r--;
+        }
+        set.insert(rh.get(l, r));
+    }
+    std::cout << set.size() << '\n';
+}
