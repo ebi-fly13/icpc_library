b2e9f60c607f3a2ec69e7d3b5d5a644ad68c90d4
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-04-21 13:11:55 +0900

    add parsing template

---
+++ string/parsing_template.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "../template/template.hpp"
+
+namespace lib {
+
+typedef std::string::const_iterator State;
+
+// *begin == expectedの判定
+bool expect(State &begin, char expected) {
+    if(*begin == expected) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// beginがexpectedを指していたらbeginを一つ進める。
+void consume(State &begin, char expected) {
+    if (*begin == expected) {
+        begin++;
+    } else {
+        assert(0);
+    }
+}
+
+bool isdigit(char c) {
+    return '0' <= c && c <= '9';
+}
+
+bool isAlpha(char c) {
+    return 'A' <= c && c <= 'Z';
+}
+
+bool isalpha(char c) {
+    return 'a' <= c && c <= 'z';
+}
+
+}
