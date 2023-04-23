3fc164c8950bf2094a785cdb6b0ab89205e6de8c
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-04-23 15:23:18 +0900

    add segtree

---
+++ data_structure/segtree.hpp
@@ -0,0 +1,107 @@
+#pragma once
+
+#include "../template/template.hpp"
+
+namespace lib {
+
+template <class S, S (*op)(S, S), S (*e)()>
+struct segtree {
+   private:
+    int n;
+    int sz;
+    std::vector<S> data;
+
+    void update(int i) { data[i] = op(data[2 * i], data[2 * i + 1]); }
+
+   public:
+    segtree(int n) : segtree(std::vector<S>(n, e())) {}
+    segtree(const std::vector<S> &v) : n((int)v.size()), sz(1) {
+        while (sz < n) sz *= 2;
+        data = std::vector<S>(2 * sz, e());
+        rep(i, 0, n) { data[sz + i] = v[i]; }
+        rrep(i, 1, sz) update(i);
+    }
+
+    void set(int p, S x) {
+        assert(0 <= p && p < n);
+        p += sz;
+        data[p] = x;
+        while (p > 1) {
+            p >>= 1;
+            update(p);
+        }
+    }
+
+    S get(int p) {
+        assert(0 <= p && p < n);
+        return data[p + sz];
+    }
+
+    S prod(int l, int r) {
+        assert(0 <= l && l <= r && r <= n);
+        S sml = e(), smr = e();
+        l += sz;
+        r += sz;
+        while (l < r) {
+            if (l & 1) sml = op(sml, data[l++]);
+            if (r & 1) smr = op(data[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(sml, smr);
+    }
+
+    S all_prod() { return data[1]; }
+
+    template <class F>
+    int max_right(int l, F f) {
+        assert(0 <= l && l < n);
+        assert(f(e()));
+        if (l == n) return n;
+        l += sz;
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(op(sm, data[l]))) {
+                while (l < sz) {
+                    l = 2 * l;
+                    if (f(op(sm, data[l]))) {
+                        sm = op(sm, data[l]);
+                        l++;
+                    }
+                }
+                return l - sz;
+            }
+            sm = op(sm, data[l]);
+            l++;
+        } while ((l & -l) != l);
+        return n;
+    }
+
+    template <class F>
+    int min_left(int r, F f) {
+        assert(0 <= r && r <= n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        r += sz;
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(op(data[r], sm))) {
+                while (r < sz) {
+                    r = 2 * r + 1;
+                    if (f(op(data[r], sm))) {
+                        sm = op(data[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - sz;
+            }
+            sm = op(data[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+};
+
+}  // namespace lib
---
+++ test/data_structure/Point_Add_Range_Sum.test.cpp
@@ -0,0 +1,37 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+
+#include "../../template/template.hpp"
+#include "../../data_structure/segtree.hpp"
+
+ll op(ll a, ll b) {
+    return a + b;
+}
+
+ll e() {
+    return 0;
+}
+
+int main() {
+    int n,q;
+    std::cin >> n >> q;
+    std::vector<ll> a(n);
+    rep(i,0,n) {
+        std::cin >> a[i];
+    }
+    lib::segtree<ll, op, e> seg(a);
+    while(q--) {
+        int t;
+        std::cin >> t;
+        if(t == 0) {
+            int p;
+            ll x;
+            std::cin >> p >> x;
+            seg.set(p, seg.get(p) + x);
+        }
+        else {
+            int l,r;
+            std::cin >> l >> r;
+            std::cout << seg.prod(l, r) << '\n';
+        }
+    }
+}
