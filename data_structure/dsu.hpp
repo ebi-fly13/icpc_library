aec6ff75b1ebec4c242611a8a45ba231c277ce87
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2023-04-24 21:22:19 +0900

    Create dsu.hpp

---
+++ data_structure/dsu.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include"../template/template.hpp"
+
+namespace lib{
+
+struct dsu {
+  public:
+    dsu(int n = 0) : _n(n), pos(n, -1) {}
+
+    int merge(int a, int b) {
+        int x = leader(a), y = leader(b);
+        if (x == y) return x;
+        if (-pos[x] < -pos[y]) swap(x, y);
+        pos[x] += pos[y];
+        pos[y] = x;
+        return x;
+    }
+
+    bool same(int a, int b) {
+        return leader(a) == leader(b);
+    }
+
+    int leader(int a) {
+        if (pos[a] < 0) return a;
+        return pos[a] = leader(pos[a]);
+    }
+
+    int size(int a) {
+        return -pos[leader(a)];
+    }
+
+  private:
+    int _n;
+    vector<int> pos;
+};
+
+} // namespace noya2
