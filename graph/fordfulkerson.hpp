f81840cdb6155f6ae6e5e89c7cc3ef3b22f1291c
shobon <shobonvip@gmail.com>
2023-04-26 00:18:42 +0900

    Update fordfulkerson.hpp

--- graph/fordfulkerson.hpp
+++ graph/fordfulkerson.hpp
@@ -4,29 +4,29 @@
 namespace lib{
 using namespace std;
 
-template<typename ft>
-struct FordFulkerson {
-    struct edge {int to; ft cap; int rev; bool isrev;};
-
-    const ft INF;
+template<typename fa>
+struct FordFulkerson{
+    struct edge {int to; fa cap; int rev; bool isrev;};
+    
+    const fa INF;
     vector<vector<edge>> g;
     vector<int> used;
     int ts;
-
-    explicit FordFulkerson(int V) : INF(numeric_limits<ft>::max()), g(V), used(V,-1), ts(0) {}
-
-    void add_edge(int from, int to, ft cap) {
+    
+    explicit FordFulkerson(int v): INF(numeric_limits<fa>::max()), g(v), used(v, -1), ts(0) {}
+    
+    void add_edge(int from ,int to, fa cap){
         g[from].push_back((edge){to, cap, (int)g[to].size(), false});
         g[to].push_back((edge){from, 0, (int)g[from].size()-1, true});
+        
     }
-
-    ft dfs(int v, const int t, ft f) {
-        if(v == t) return f;
+    fa dfs(int v, int t, fa flow){
+        if (v==t) return flow;
         used[v] = ts;
-        for(auto &e: g[v]) {
-            if(e.cap > 0 && used[e.to] != ts) {
-                ft d = dfs(e.to, t, min(f, e.cap));
-                if(d > 0) {
+        for(auto &e : g[v]){
+            if(e.cap > 0 && used[e.to] != ts){
+                fa d = dfs(e.to, t, min(flow, e.cap));
+                if(d > 0){
                     e.cap -= d;
                     g[e.to][e.rev].cap += d;
                     return d;
@@ -36,9 +36,9 @@ struct FordFulkerson {
         return 0;
     }
 
-    ft flow(int s, int t) {
-        ft ret = 0;
-        for(ft f; (f=dfs(s,t,INF))>0; ts++) ret += f;
+    fa flow(int s, int t){
+        fa ret = 0;
+        for (fa f; (f = dfs(s, t, INF)) > 0; ts++) ret += f;
         ts++;
         return ret;
     }
