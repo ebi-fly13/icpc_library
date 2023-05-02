be51b90c5a472fbf14f671ffb27337de6128a8c2
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2023-05-02 22:40:20 +0900

    Update convex_hull.hpp

--- geometry/convex_hull.hpp
+++ geometry/convex_hull.hpp
@@ -14,41 +14,33 @@ vector<vec> convex_hull(vector<vec> a){
     };
     sort(all(a),comp);
     stack<int> uid, did;
-    uid.push(0);
     vec ri = a[n-1];
-    rep(i,1,n-1) {
-        vec le = a[uid.top()];
-        if (cross(ri-le,a[i]-le) > 0){
-            while (uid.size() >= 2){
-                int test = uid.top(); uid.pop();
-                vec from = a[uid.top()];
-                if (cross(a[i]-from,a[test]-from) > 0){
-                    uid.push(test);
-                    break;
+    auto make_half = [&](bool isupper){
+        auto &id = (isupper ? uid : did);
+        id.push(0);
+        rep(i,1,n-1){
+            vec le = a[id.top()];
+            auto cr = cross(ri-le,a[i]-le);
+            if ((cr > 0 && isupper) || (cr < 0 && !isupper)){
+                while (id.size() >= 2){
+                    int test = id.top(); id.pop();
+                    vec from = a[id.top()];
+                    auto cr2 = cross(a[i]-from,a[test]-from);
+                    if ((cr2 > 0 && isupper) || (cr2 < 0 && !isupper)){
+                        id.push(test);
+                        break;
+                    }
                 }
+                id.push(i);
             }
-            uid.push(i);
         }
-    }
-    did.push(0);
-    rep(i,1,n-1){
-        vec le = a[did.top()];
-        if (cross(ri-le,a[i]-le) < 0){
-            while (did.size() >= 2){
-                int test = did.top(); did.pop();
-                vec from = a[did.top()];
-                if (cross(a[i]-from,a[test]-from) < 0){
-                    did.push(test);
-                    break;
-                }
-            }
-            did.push(i);
-        }
-    }
+    };
+    make_half(true); make_half(false);
     vector<int> ids(1,n-1);
-    while (!uid.empty()) ids.emplace_back(uid.top()), uid.pop();
-    reverse(all(ids));
     while (!did.empty()) ids.emplace_back(did.top()), did.pop();
+    reverse(all(ids));
+    while (!uid.empty()) ids.emplace_back(uid.top()), uid.pop();
+    ids.pop_back();
     vector<vec> ans(ids.size());
     rep(i,0,ids.size()) ans[i] = a[ids[i]];
     return ans;
