e412f0d902168dffbe29d9dc0fc90bfd12e12fa7
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-05-14 18:21:59 +0900

    add dsu test

---
+++ test/data_structure/Unionfind.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include "../../data_structure/dsu.hpp"
+#include "../../template/template.hpp"
+
+int main() {
+    int n, q;
+    std::cin >> n >> q;
+    lib::dsu uf(n);
+    while (q--) {
+        int t, u, v;
+        std::cin >> t >> u >> v;
+        if (t == 0) {
+            uf.merge(u, v);
+        } else {
+            std::cout << (int)uf.same(u, v) << '\n';
+        }
+    }
+}
