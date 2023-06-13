1d721c9cb3218100d548fddd31f7a524f9484def
shobon <shobonvip@gmail.com>
2023-06-13 18:10:34 +0900

    Create Minimum_Flow.test.cpp

---
+++ test/graph/Minimum_Flow.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM \
+	"https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_6_B"
+
+#include "../../graph/mcf_graph.hpp"
+#include "../../template/template.hpp"
+
+int main(){
+	int v,e,f;
+	std::cin>>v>>e>>f;
+	lib::MinCostFlow mcf(v);
+	for (int i=0;i<e;i++){
+		int a,b,c,d;
+		std::cin >> a >> b >> c >> d;
+		mcf.add_edge(a,b,c,d);
+	}
+	ll ans = mcf.flow(0,v-1,f);
+	std::cout << ans << std::endl;
+}
