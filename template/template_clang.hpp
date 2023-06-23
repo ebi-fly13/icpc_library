372f119bc99d0c235919418d7cc00c338e778ebc
Hiroto Yano <52492037+ebi-fly13@users.noreply.github.com>
2023-06-23 22:08:03 +0900

    Update template_clang.hpp

--- template/template_clang.hpp
+++ template/template_clang.hpp
@@ -18,3 +18,24 @@
 #include <unordered_set>
 #include <utility>
 #include <vector>
+
+#define rep(i, s, n) for (int i = (int)(s); i < (int)(n); i++)
+#define rrep(i, s, n) for (int i = (int)(n)-1; i >= (int)(s); i--)
+#define all(v) v.begin(), v.end()
+
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+
+template <typename T> bool chmin(T &a, const T &b) {
+    if (a <= b) return false;
+    a = b;
+    return true;
+}
+template <typename T> bool chmax(T &a, const T &b) {
+    if (a >= b) return false;
+    a = b;
+    return true;
+}
+
+using namespace std;
