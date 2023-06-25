6bd3295f36a1754a1e98c813ff490aae273a2b47
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-06-26 01:40:24 +0900

    add segtree 2d

---
+++ data_structure/segtree_2d.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "../template/template.hpp"
+
+namespace lib {
+
+template <class S, S (*op)(S, S), S (*e)(), class data_structure>
+struct segtree_2d {
+  private:
+  public:
+    segtree_2d(int h, int w) : h(h), w(w), sz(1) {
+        while (sz < h) sz <<= 1;
+        data = std::vector<data_structure>(2 * sz, data_structure(w));
+    }
+
+    void set(int i, int j, S x) {
+        i += sz;
+        data[i].set(j, x);
+        while (i > 1) {
+            i >>= 1;
+            S val = op(data[2 * i].get(j), data[2 * i + 1].get(j));
+            data[i].set(j, val);
+        }
+    }
+
+    S get(int i, int j) {
+        assert(0 <= i && i < h && 0 <= j && j < w);
+        return data[i + sz].get(j);
+    }
+
+    S prod(int l, int d, int r, int u) {
+        assert(0 <= l && l <= r && r <= h);
+        assert(0 <= d && d <= u && u <= w);
+        l += sz;
+        r += sz;
+        S res = e();
+        while (l < r) {
+            if (l & 1) res = op(res, data[l++].prod(d, u));
+            if (r & 1) res = op(data[--r].prod(d, u), res);
+            l >>= 1;
+            r >>= 1;
+        }
+        return res;
+    }
+
+  private:
+    int h, w;
+    int sz;
+    std::vector<data_structure> data;
+};
+
+}  // namespace lib
---
+++ test/data_structure/aoj_1068.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1068"
+
+#include "../../data_structure/segtree.hpp"
+#include "../../data_structure/segtree_2d.hpp"
+#include "../../template/template.hpp"
+
+using namespace lib;
+
+int op(int a, int b) {
+    return a < b ? a : b;
+}
+
+int e() {
+    return std::numeric_limits<int>::max();
+}
+
+int main() {
+    int r, c, q;
+    while (std::cin >> r >> c >> q, !(r == 0 && c == 0 && q == 0)) {
+        segtree_2d<int, op, e, segtree<int, op, e>> seg2d(r, c);
+        rep(i, 0, r) {
+            rep(j, 0, c) {
+                int grid;
+                std::cin >> grid;
+                seg2d.set(i, j, grid);
+            }
+        }
+        while (q--) {
+            int l, d, r, u;
+            std::cin >> l >> d >> r >> u;
+            r++;
+            u++;
+            std::cout << seg2d.prod(l, r, d, u) << '\n';
+        }
+    }
+}
