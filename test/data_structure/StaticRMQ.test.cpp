092bbffee3119832785338b7d5fc37bcc8cb33c5
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2023-06-29 01:49:53 +0900

    Create StaticRMQ.test.cpp

---
+++ test/data_structure/StaticRMQ.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+
+#include"../../template/template.hpp"
+
+#include"../../data_structure/sparce_table.hpp"
+
+using namespace lib;
+
+int op(int a, int b){
+    return min(a,b);
+}
+
+int main(){
+    int n, q; cin >> n >> q;
+    vector<int> a(n);
+    rep(i,0,n) cin >> a[i];
+    SparseTable<int,op> spt(a);
+    while (q--){
+        int l, r; cin >> l >> r;
+        cout << spt.get(l,r) << '\n';
+    }
+}
