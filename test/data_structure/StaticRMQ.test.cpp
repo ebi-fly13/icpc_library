ea4eb9f3e4ad3cf4b9336201194d69f849512205
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-06-29 14:53:09 +0900

    format

--- test/data_structure/StaticRMQ.test.cpp
+++ test/data_structure/StaticRMQ.test.cpp
@@ -1,22 +1,23 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
 
-#include"../../template/template.hpp"
-
-#include"../../data_structure/sparse_table.hpp"
+#include "../../data_structure/sparse_table.hpp"
+#include "../../template/template.hpp"
 
 using namespace lib;
 
-int op(int a, int b){
-    return min(a,b);
+int op(int a, int b) {
+    return min(a, b);
 }
 
-int main(){
-    int n, q; cin >> n >> q;
+int main() {
+    int n, q;
+    cin >> n >> q;
     vector<int> a(n);
-    rep(i,0,n) cin >> a[i];
-    SparseTable<int,op> spt(a);
-    while (q--){
-        int l, r; cin >> l >> r;
-        cout << spt.get(l,r) << '\n';
+    rep(i, 0, n) cin >> a[i];
+    SparseTable<int, op> spt(a);
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        cout << spt.get(l, r) << '\n';
     }
 }
