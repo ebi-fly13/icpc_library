874cb835eb65613e8c33178d38849aa4857c2844
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-06-29 15:06:40 +0900

    fix

--- data_structure/sparse_table.hpp
+++ data_structure/sparse_table.hpp
@@ -1,43 +1,44 @@
 #pragma once
 
-#include"../template/template.hpp"
+#include "../template/template.hpp"
 
-namespace lib{
+namespace lib {
 
-template<class S, S (*op)(S, S)>
-struct SparseTable{
+template <class S, S (*op)(S, S)> struct SparseTable {
     vector<vector<S>> table;
     vector<int> clz;
-    SparseTable (const vector<S> &vec){
+    SparseTable(const vector<S> &vec) {
         int n = vec.size(), n2 = 0;
-        while ((1<<n2) < n) n2++;
-        table.resize(n2+1);
-        rep(i,0,n2+1){
+        while ((1 << n2) < n) n2++;
+        table.resize(n2 + 1);
+        rep(i, 0, n2 + 1) {
             table[i].resize(n);
-            if (i == 0){
-                rep(j,0,n) table[i][j] = vec[j];
-            }
-            else {
-                rep(j,0,n){
-                    if (j + (1<<(i-1)) < n) table[i][j] = op(table[i-1][j],table[i-1][j + (1<<(i-1))]);
-                    else table[i][j] = table[i-1][j];
+            if (i == 0) {
+                rep(j, 0, n) table[i][j] = vec[j];
+            } else {
+                rep(j, 0, n) {
+                    if (j + (1 << (i - 1)) < n)
+                        table[i][j] = op(table[i - 1][j], table[i - 1][j + (1 << (i - 1))]);
+                    else
+                        table[i][j] = table[i - 1][j];
                 }
             }
         }
-        clz.resize(n+1);
-        rep(i,0,n2+1){
-            for (int j = (1<<i); j < (2<<i) && j <= n; j++){
+        clz.resize(n + 1);
+        rep(i, 0, n2 + 1) {
+            for (int j = (1 << i); j < (2 << i) && j <= n; j++) {
                 clz[j] = i;
             }
         }
     }
-    // 単位元を要求しないので if (l >= r) return e() みたいなことをしていない、注意すること！！
-    S get(int l, int r){
+    // 単位元を要求しないので if (l >= r) return e()
+    // みたいなことをしていない、注意すること！！
+    S get(int l, int r) {
         assert(r - l > 0);
-        //int lgs = 31 - __builtin_clz((unsigned int)(r-l));
-        int lgs = clz[r-l];
+        // int lgs = 31 - __builtin_clz((unsigned int)(r-l));
+        int lgs = clz[r - l];
         return op(table[lgs][l], table[lgs][r - (1 << lgs)]);
     }
 };
 
-} // namespace lib
+}  // namespace lib
