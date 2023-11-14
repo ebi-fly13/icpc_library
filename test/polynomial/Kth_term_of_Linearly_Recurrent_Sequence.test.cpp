f4863646f37e2da7a0495e48fef0dc497fc09aa1
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2023-11-14 20:46:04 +0900

    add bostan mori

---
+++ test/polynomial/Kth_term_of_Linearly_Recurrent_Sequence.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM \
+    "https://judge.yosupo.jp/problem/kth_term_of_linearly_recurrent_sequence"
+
+#include "../../convolution/ntt.hpp"
+#include "../../fps/bostan_mori.hpp"
+#include "../../template/template.hpp"
+#include "../../utility/modint.hpp"
+
+using namespace lib;
+using mint = modint998244353;
+
+int main() {
+    int d;
+    long long k;
+    std::cin >> d >> k;
+    std::vector<mint> a(d), c(d);
+    for (int i = 0; i < d; i++) {
+        int val;
+        std::cin >> val;
+        a[i] = val;
+    }
+    for (int i = 0; i < d; i++) {
+        int val;
+        std::cin >> val;
+        c[i] = val;
+    }
+    std::cout << kitamasa<mint, convolution>(k, a, c).val() << '\n';
+}
